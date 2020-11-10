b64aeaa9ea8536dd78ebd9dad3874cc8ec96cb35
Jaclyn Tan <jaclyntan7@gmail.com>
2020-11-10 18:29:12 +0800

    💀

--- src/_data/site.js
+++ src/_data/site.js
@@ -12,6 +12,6 @@ module.exports = {
   url: 'https://monkmedia.com.au',
   image: 'https://avatars3.githubusercontent.com/u/447956?s=400&v=4',
   description: 'Just another static site',
-  devUrl: 'http://localhost:8080',
+  baseUrl: 'http://localhost:8080',
   repo:'https://github.com/monkstudio/unicorn-fluff'
 };
